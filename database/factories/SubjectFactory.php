140d513f4d319c103de57df6943641d85648383d
jinyao44 <leejinyao1112@gmail.com>
2026-01-16 11:10:47 +0800

    Weak Cryptography Fix #6
    random_int

--- database/factories/SubjectFactory.php
+++ database/factories/SubjectFactory.php
@@ -6,7 +6,7 @@ $factory->define(App\Subject::class, function (Faker $faker) {
     return [
         'name' => $faker->sentence($nbWords = 3, $variableNbWords = true),
         'code' => $faker->unique()->numberBetween($min = 100, $max = 200),
-        'type' => rand(1,2),
+        'type' => random_int(1,2),
         'class_id' => function () {
             // Get random class id
             return App\IClass::where('id','!=',1)->where('id','!=',2)->inRandomOrder()->first()->id;
