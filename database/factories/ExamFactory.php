51295f293fc1737f7e4a1ffa2276dbaff44125ad
jinyao44 <leejinyao1112@gmail.com>
2026-01-16 10:59:03 +0800

    Weak Cryptography Fix #1
    Use random_int for elective_subject_point_addition

--- database/factories/ExamFactory.php
+++ database/factories/ExamFactory.php
@@ -5,7 +5,7 @@ use Faker\Generator as Faker;
 $factory->define(App\Exam::class, function (Faker $faker) {
     return [
         'name' => $faker->sentence($nbWords = 3, $variableNbWords = true),
-        'elective_subject_point_addition' => rand(0,2),
+        'elective_subject_point_addition' => random_int(0,2),
         'marks_distribution_types' => json_encode(array_rand(\App\Http\Helpers\AppHelper::MARKS_DISTRIBUTION_TYPES, 3)),
         'class_id' => function () {
             // Get random class id
